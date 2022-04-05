b0336abebf97530c30d0a741c12ceac632d9f780
Mateus Fonseca de Souza <mateusfonsecas1999@gmail.com>
2022-04-05 15:12:45 -0300

    correct thread

--- main.py
+++ main.py
@@ -15,6 +15,83 @@ import numpy as np
 
 import threading
 
+
+
+janela = tk.Tk()
+janela.geometry("700x300")
+janela.title("Interface em desenvolvimento")
+
+a = 10
+b = 30
+c = 10
+
+
+
+texto = ttk.Label(janela, text="Escolha modo de exibição do frame capturado:")
+texto.place(height=20, width=250, x=a, y=(c + 5*b))
+
+texto2 = ttk.Label(janela, text="Defina os parâmetros para exibição em tempo real:")
+texto2.place(height=20, width=300, x=a, y=(c + 2*b))
+
+
+var1 = tk.IntVar()
+check2 = ttk.Checkbutton(janela, text='Cores', variable= var1)
+check2.place(height=20, width=100, x=a, y=(c + 3*b))
+
+var2 = tk.IntVar()
+check2 = ttk.Checkbutton(janela, text='Curvas', variable= var2)
+check2.place(height=20, width=100, x=27*a, y=(c + 3*b))
+
+selected_gradient = tk.StringVar()
+lt =  ['COLORMAP_AUTUMN','COLORMAP_BONE' ,
+                       'COLORMAP_JET(PADRÃO)' ,
+                       'COLORMAP_WINTER' ,
+                       'COLORMAP_RAINBOW' ,
+                       'COLORMAP_OCEAN',
+                       'COLORMAP_SUMMER',
+                       'COLORMAP_SPRING',
+                       'COLORMAP_COOL',
+                       'COLORMAP_HSV',
+                       'COLORMAP_PINK',
+                       'COLORMAP_HOT',
+                       'COLORMAP_PARULA',
+                       'COLORMAP_MAGMA',
+                       'COLORMAP_INFERNO',
+                       'COLORMAP_PLASMA',
+                       'COLORMAP_VIRIDIS',
+                       'COLORMAP_CIVIDIS',
+                       'COLORMAP_TWILIGHT',
+                       'COLORMAP_TWILIGHT_SHIFTED',
+                       'COLORMAP_TURBO',
+                       'COLORMAP_DEEPGREEN']
+gradi = ttk.Combobox(janela, values = lt, textvariable= selected_gradient)
+gradi.set('Mapa de cor')
+gradi['state'] = 'readonly'
+gradi.place(height=20, width=195, x=7*a, y=(c + 3*b))
+
+var3 = tk.IntVar()
+list_numbers_curv= [1,3,5,8,10,15,20,30]
+numbers_curv = ttk.Combobox(janela, values = list_numbers_curv, textvariable= var3)
+numbers_curv.set('Distância entre curvas')
+numbers_curv['state'] = 'readonly'
+numbers_curv.place(height=20, width=140, x=33*a, y=(c + 3*b))
+
+
+var4 = tk.IntVar()
+list_thickness_curv = [1,2,3,4,5,6,7,8]
+thickness_curv = ttk.Combobox(janela, values =list_thickness_curv, textvariable= var4)
+thickness_curv.set('Espessura')
+thickness_curv['state'] = 'readonly'
+thickness_curv.place(height=20, width=100, x=48*a, y=(c + 3*b))
+
+var5 = tk.StringVar()
+sizlist = ['640x480','720x640','1280x720']
+siz = ttk.Combobox(janela, values = sizlist, textvariable= var5)
+siz.set('Resolução')
+siz['state'] = 'readonly'
+siz.place(height=20, width=100, x=59*a, y=(c + 3*b))
+
+
 #==========================================================================================
     #Inicializando kinect
 #==========================================================================================
@@ -32,7 +109,7 @@ depth_stream.start()
     #Definindo função para vizualização das curvas de nível
 #==========================================================================================
 
-def exibe_curvas_de_nivel(depth_stream, n_curvas_de_nivel=40, x_label=np.arange(80, 640, 1),
+def exibe_curvas_de_nivel(n_curvas_de_nivel=40, x_label=np.arange(80, 640, 1),
                           y_label=np.arange(10, 480, 1)):
     """
     Função para definir a exibição das curvas de nível na imagem.
@@ -42,6 +119,8 @@ def exibe_curvas_de_nivel(depth_stream, n_curvas_de_nivel=40, x_label=np.arange(
     :param y_label:
     :return:
     """
+    depth_stream = dev.create_depth_stream()
+    depth_stream.start()
     x_label, y_label = np.meshgrid(x_label, y_label)
     frame = depth_stream.read_frame()
     frame_data = frame.get_buffer_as_uint16()
@@ -60,7 +139,7 @@ def exibe_curvas_de_nivel(depth_stream, n_curvas_de_nivel=40, x_label=np.arange(
     #Definindo função para vizualização em 3D
 #==========================================================================================
 
-def exibe_3d(depth_stream, x_label=np.arange(80, 640, 1), y_label=np.arange(10, 480, 1), cmap=cm.coolwarm, linewidth=0,
+def exibe_3d(x_label=np.arange(80, 640, 1), y_label=np.arange(10, 480, 1), cmap=cm.coolwarm, linewidth=0,
              antialised=True):
     """
     Função para fazer a exibição da imagem em três dimensões
@@ -72,6 +151,8 @@ def exibe_3d(depth_stream, x_label=np.arange(80, 640, 1), y_label=np.arange(10, 
     :param antialised:
     :return:
     """
+    depth_stream = dev.create_depth_stream()
+    depth_stream.start()
     x_label, y_label = np.meshgrid(x_label, y_label)
 
     frame = depth_stream.read_frame()
@@ -87,11 +168,27 @@ def exibe_3d(depth_stream, x_label=np.arange(80, 640, 1), y_label=np.arange(10, 
     ax.zaxis.set_major_formatter('{x:.02f}')
     fig.colorbar(surf, shrink=0.5, aspect=5)  
     plt.show()
-    
+
+list_var = [cv2.COLORMAP_JET, 1, 1, 5, 2, 10]
+
+def maplic():
+    global list_var
+    if ((thickness_curv.current() != -1) & (siz.current() != -1 )
+                                     & (numbers_curv.current() != -1)
+                                     & (gradi.current() != -1)):
+                                     list_var = [gradi.current(),var1.get(),var2.get(),
+                                     var3.get(),var4.get(), siz.current()]
+                                     botao1["state"] = tk.NORMAL
+
+    else:
+        tk.messagebox.showerror("Erro", "Defina todos os paramêtros")
 
 
-def exib_TR(depth_stream, mapoption = cv2.COLORMAP_JET, walloption=1, curv = 1, n=5, thicknesscurv= 2, siz = 10):
-    
+def exib_TR(mapoption = list_var[0], walloption= list_var[1], curv = list_var[2], n=list_var[3], 
+                                                            thicknesscurv= list_var[4], siz = list_var[5]):
+    botao1["state"] = tk.DISABLED
+    depth_stream = dev.create_depth_stream()
+    depth_stream.start()
     def onMouse(event, x, y, flags, param):
         global dist   
         if event == cv2.EVENT_MOUSEMOVE:
@@ -156,6 +253,7 @@ def exib_TR(depth_stream, mapoption = cv2.COLORMAP_JET, walloption=1, curv = 1, 
         cv2.waitKey(34)
       
         if cv2.getWindowProperty("Curvas em tempo real com mapa de cores", cv2.WND_PROP_VISIBLE) <1:
+            botao1["state"] = tk.NORMAL
             break
 
 
@@ -169,102 +267,21 @@ def m1():
                                      & (gradi.current() != -1)):
                                      x = threading.Thread(target=exib_TR(depth_stream, 
                                        gradi.current(),var1.get(),var2.get(),var3.get(),var4.get(), siz.current()))
-                                     x.start()
+                                     x.start
     else:
         tk.messagebox.showerror("Erro", "Defina todos os paramêtros")
 
-def m4():
-    x = threading.Thread(target= exibe_curvas_de_nivel(depth_stream, n_curvas_de_nivel=40,
-                         x_label=np.arange(80, 640, 1), y_label=np.arange(10, 480, 1)))
-    x.start()
-
-def m5():
-    x = threading.Thread(target=exibe_3d(depth_stream, x_label=np.arange(80, 640, 1),
-                      y_label=np.arange(10, 480, 1), cmap=cm.coolwarm, linewidth=0, antialised=True))
-    x.start()
-#==========================================================================================
-    #Criando interface gráfica
-#==========================================================================================
-
-
-janela = tk.Tk()
-janela.geometry("700x300")
-janela.title("Interface em desenvolvimento")
-
-a = 10
-b = 30
-c = 10
-
-botao1 = ttk.Button(janela, text="Exibir", command= m1 )
+botao1 = ttk.Button(janela, text="Exibir", command= lambda:threading.Thread(target=exib_TR, args= (list_var[0],list_var[1],
+                                       list_var[2],list_var[3],list_var[4], list_var[5])).start())                                
 botao1.place(height=20, width=50, x=a, y=(c + 4*b))
 
-texto = ttk.Label(janela, text="Escolha modo de exibição do frame capturado:")
-texto.place(height=20, width=250, x=a, y=(c + 5*b))
+botao2 = ttk.Button(janela, text="Exibir curvas", command= lambda: threading.Thread(target= exibe_curvas_de_nivel).start())
+botao2.place(height=20, width=80,x=a, y=(c + 6*b))
 
-texto2 = ttk.Label(janela, text="Defina os parâmetros para exibição em tempo real:")
-texto2.place(height=20, width=300, x=a, y=(c + 2*b))
+botao3 = ttk.Button(janela, text="Exibir superficie", command= lambda: threading.Thread(target=exibe_3d).start())
+botao3.place(height=20, width=100, x=a, y=(c + 7*b))
 
-botao4 = ttk.Button(janela, text="Exibir curvas", command= m4)
-botao4.place(height=20, width=80,x=a, y=(c + 6*b))
-
-botao5 = ttk.Button(janela, text="Exibir superficie", command=m5)
-botao5.place(height=20, width=100, x=a, y=(c + 7*b))
-
-var1 = tk.IntVar()
-check2 = ttk.Checkbutton(janela, text='Cores', variable= var1)
-check2.place(height=20, width=100, x=a, y=(c + 3*b))
-
-var2 = tk.IntVar()
-check2 = ttk.Checkbutton(janela, text='Curvas', variable= var2)
-check2.place(height=20, width=100, x=27*a, y=(c + 3*b))
-
-selected_gradient = tk.StringVar()
-lt =  ['COLORMAP_AUTUMN','COLORMAP_BONE' ,
-                       'COLORMAP_JET(PADRÃO)' ,
-                       'COLORMAP_WINTER' ,
-                       'COLORMAP_RAINBOW' ,
-                       'COLORMAP_OCEAN',
-                       'COLORMAP_SUMMER',
-                       'COLORMAP_SPRING',
-                       'COLORMAP_COOL',
-                       'COLORMAP_HSV',
-                       'COLORMAP_PINK',
-                       'COLORMAP_HOT',
-                       'COLORMAP_PARULA',
-                       'COLORMAP_MAGMA',
-                       'COLORMAP_INFERNO',
-                       'COLORMAP_PLASMA',
-                       'COLORMAP_VIRIDIS',
-                       'COLORMAP_CIVIDIS',
-                       'COLORMAP_TWILIGHT',
-                       'COLORMAP_TWILIGHT_SHIFTED',
-                       'COLORMAP_TURBO',
-                       'COLORMAP_DEEPGREEN']
-gradi = ttk.Combobox(janela, values = lt, textvariable= selected_gradient)
-gradi.set('Mapa de cor')
-gradi['state'] = 'readonly'
-gradi.place(height=20, width=195, x=7*a, y=(c + 3*b))
-
-var3 = tk.IntVar()
-list_numbers_curv= [1,3,5,8,10,15,20,30]
-numbers_curv = ttk.Combobox(janela, values = list_numbers_curv, textvariable= var3)
-numbers_curv.set('Distância entre curvas')
-numbers_curv['state'] = 'readonly'
-numbers_curv.place(height=20, width=140, x=33*a, y=(c + 3*b))
-
-
-var4 = tk.IntVar()
-list_thickness_curv = [1,2,3,4,5,6,7,8]
-thickness_curv = ttk.Combobox(janela, values =list_thickness_curv, textvariable= var4)
-thickness_curv.set('Espessura')
-thickness_curv['state'] = 'readonly'
-thickness_curv.place(height=20, width=100, x=48*a, y=(c + 3*b))
-
-var5 = tk.StringVar()
-sizlist = ['640x480','720x640','1280x720']
-siz = ttk.Combobox(janela, values = sizlist, textvariable= var5)
-siz.set('Resolução')
-siz['state'] = 'readonly'
-siz.place(height=20, width=100, x=59*a, y=(c + 3*b))
+botao4 = ttk.Button(janela, text="Aplicar", command= maplic)                                 
+botao4.place(height=20, width=50, x=10*a, y=(c + 4*b))
 
 janela.mainloop()
